9b2c3bd7e2cbf763c28ffa077354250c3b3cfe2f
almen1 <151327599+almen1@users.noreply.github.com>
2024-04-21 19:15:34 +0800

    updated UI

--- index.php
+++ index.php
@@ -6,90 +6,37 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>HOPE</title>
         <link rel="stylesheet" href="./css/landing.css">
-        <link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300..800;1,300..800&display=swap" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css2?family=Inter:wght@100..900&display=swap" rel="stylesheet">
     </head>
 
     <body>
         <nav class="navbar">
             <div class="nav-icon">
-
             </div>
             <div class="nav-elements">
                 <ul class="nav-list">
                     <li><a href="">Home</a></li>
                     <li><a href="">About Us</a></li>
-                    <li><a href="">Contact Us</a></li>
+                    <li><a href="">Get Involved</a></li>
                 </ul>
             </div>
         </nav>
+
         <div class="container">
             <div class="hero">
                 <div class="hero-bg">
-                    <div class="hero-title">HOPE</div>
                 </div>
-            </div>
-            <div class="card-track">
-                <div class="card">
-                    <p>About Us</p>
-                </div>
-                <div class="card">
-                    <p>What we do</p>
-                </div>
-                <div class="card">
-                    <p>→</p>
-                </div>
-                <div class="card">
-                    <p>NO <br>POVERTY</p>
-                </div>
-                <div class="card">
-                    <p>ZERO <br>HUNGER</p>
-                </div>
-                <div class="card">
-                    <p>GOOD HEALTH <br>AND WELL-BEING</p>
-                </div>
-                <div class="card">
-                    <p>QUALITY <br>EDUCATION</p>
-                </div>
-                <div class="card">
-                    <p>GENDER <br>EQUALITY</p>
-                </div>
-                <div class="card">
-                    <p>CLEAN WATER <br>AND SANITATION</p>
-                </div>
-                <div class="card">
-                    <p>AFFORDABLE <br>AND CLEAN ENERGY</p>
-                </div>
-                <div class="card">
-                    <p>DECENT WORK <br>AND ECONOMIC GROWTH</p>
-                </div>
-                <div class="card">
-                    <p>INDUSTRY, INNOVATION <br>AND INFRASTRUCTURE</p>
-                </div>
-                <div class="card">
-                    <p>REDUCED <br>INEQUALITIES</p>
-                </div>
-                <div class="card">
-                    <p>SUSTAINABLE CITIES <br>AND COMMUNITIES </p>
-                </div>
-                <div class="card">
-                    <p>RESPONSIBLE CONSUMPTION <br>AND PRODUCTION</p>
-                </div>
-                <div class="card">
-                    <p>CLIMATE <br>ACTION</p>
-                </div>
-                <div class="card">
-                    <p>LIFE <br>BELOW WATER</p>
-                </div>
-                <div class="card">
-                    <p>LIFE <br>ON LAND</p>
-                </div>
-                <div class="card">
-                    <p>PEACE, JUSTICE <br>AND STRONG INSTITUTIONS</p>
-                </div>
-                <div class="card">
-                    <p>PARTNERSHIP <br>FOR THE GOALS</p>
+                <div class="hero-elements">
+                    <p id="hero-title">HOPE</p>
+                    <p id="hero-sub">Every Act Counts</p>
+                    <button>
+                        <p>Read More</p>
+                    </button>
                 </div>
             </div>
         </div>
     </body>
+    <script>
+    </script>
+
     </html>
