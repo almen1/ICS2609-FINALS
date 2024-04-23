643dc01c3a1726d1614c5fdada80e83d63b9c652
almen1 <151327599+almen1@users.noreply.github.com>
2024-04-23 11:10:07 +0800

    updated UI

--- index.php
+++ index.php
@@ -9,7 +9,8 @@
         <link href="https://fonts.googleapis.com/css2?family=Inter:wght@100..900&display=swap" rel="stylesheet">
     </head>
 
-    <body>
+    <body data-card="false">
+
         <nav class="navbar">
             <div class="nav-icon">
             </div>
@@ -22,21 +23,39 @@
             </div>
         </nav>
 
+        <main>
+            <div class="hero-title">
+            </div>
+        </main>
+
         <div class="container">
-            <div class="hero">
-                <div class="hero-bg">
-                </div>
-                <div class="hero-elements">
-                    <p id="hero-title">HOPE</p>
-                    <p id="hero-sub">Every Act Counts</p>
-                    <button>
-                        <p>Read More</p>
-                    </button>
-                </div>
+            <div id="card-track">
+                <a class="card-link" href="#">
+                    <p class="card-link-label">NO POVERTY</p>
+                    <img class="card-link-image" src="./assets/hope-sdg1.svg" />
+                </a>
+                <a class="card-link" href="#">
+                    <p class="card-link-label">ZERO HUNGER</p>
+                    <img class="card-link-image" src="./assets/hope-sdg2.svg" />
+                </a>
+                <a class="card-link" href="#">
+                    <p class="card-link-label">GOOD HEALTH</p>
+                    <img class="card-link-image" src="./assets/hope-sdg3.svg" />
+                </a>
             </div>
         </div>
+
+        <button id="card-toggle" type="button" onclick="toggleCard()">↻
+            <p class="open"></p>
+            <p class="close"></p>
+        </button>
+
+
     </body>
     <script>
+        const toggleCard = () => {
+            document.body.dataset.card = document.body.dataset.card === "true" ? "false" : "true";
+        }
     </script>
 
     </html>
