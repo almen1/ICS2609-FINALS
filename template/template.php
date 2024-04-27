aff85a0954cc30d6b05efcd3843c1ce38a7d8eed
almen1 <151327599+almen1@users.noreply.github.com>
2024-04-27 14:57:19 +0800

    updated template.php

--- template/template.php
+++ template/template.php
@@ -1,11 +1,54 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../css/template.css">
+    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@100..900&display=swap" rel="stylesheet">
     <title>HOPE</title>
 </head>
+
 <body>
-    TEST
+    <nav class="navbar">
+        <div class="nav-icon">
+        </div>
+        <div class="nav-elements">
+            <ul class="nav-list">
+                <li><a href="../index.php">Home</a></li>
+                <li><a href="../index.php">About Us</a></li>
+                <li><a href="../index.php">Get Involved</a></li>
+            </ul>
+        </div>
+    </nav>
+
+    <main>
+        <div class="hero">
+            <div class="hero-elements">
+
+            </div>
+        </div>
+
+        <div class="web-info1">
+            <div class="web-title1">
+
+            </div>
+            <div class="web-desc1">
+
+            </div>
+        </div>
+        <div class="web-info2">
+            <div class="web-title2">
+
+            </div>
+            <div class="web-desc2">
+
+            </div>
+        </div>
+        <div class="donate-btn">
+
+        </div>
+    </main>
 </body>
+
 </html>
