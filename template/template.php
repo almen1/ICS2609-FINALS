411a4af0fc9a9b81ac269857e9cf8de6bffc4880
almen1 <aeronalmendras@gmail.com>
2024-05-02 21:34:40 +0800

    updated template background

--- template/template.php
+++ template/template.php
@@ -1,3 +1,28 @@
+<?php
+$sdgImages = array(
+    "ZERO POVERTY" => "hope-sdg1.jpg",
+    "HUNGER RELIEF" => "hope-sdg2.jpg",
+    "HEALTH ACCESS" => "hope-sdg3.jpg",
+    "EDUCATION EQUITY" => "hope-sdg4.jpg",
+    "GENDER EMPOWERMENT" => "hope-sdg5.jpg",
+    "WATER SECURITY" => "hope-sdg6.jpg",
+    "ENERGY ACCESSIBILITY" => "hope-sdg7.jpg",
+    "JOB OPPORTUNITIES" => "hope-sdg8.jpg",
+    "INNOVATION SUPPORT" => "hope-sdg9.jpg",
+    "SOCIAL INCLUSION" => "hope-sdg10.jpg",
+    "URBAN SUSTAINABILITY" => "hope-sdg11.jpg",
+    "ETHICAL CONSUMPTION" => "hope-sdg12.jpg",
+    "CLIMATE ACTION" => "hope-sdg13.jpg",
+    "MARINE CONSERVATION" => "hope-sdg14.jpg",
+    "LAND PRESERVATION" => "hope-sdg15.jpg",
+    "SOCIAL EQUITY" => "hope-sdg16.jpg",
+    "COLLABORATIVE IMPACT" => "hope-sdg17.jpg"
+);
+
+$selected_link = isset($_GET['link']) ? $_GET['link'] : null;
+$imageName = isset($sdgImages[$selected_link]) ? $sdgImages[$selected_link] : null;
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -7,6 +32,13 @@
     <link rel="stylesheet" href="../css/template.css">
     <link href="https://fonts.googleapis.com/css2?family=Inter:wght@100..900&display=swap" rel="stylesheet">
     <title>HOPE</title>
+    <style>
+        .hero {
+            background-image: url('../assets/<?php echo $imageName; ?>');
+            background-size: cover;
+            background-repeat: no-repeat;
+        }
+    </style>
 </head>
 
 <body>
